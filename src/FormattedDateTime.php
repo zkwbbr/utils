6e96fcbcda0726d19f5df9446966710c70175d3b
zkwbbr <45949485+zkwbbr@users.noreply.github.com>
2019-03-20 01:50:48 +0800

    Improve code of FormattedDateTime::x()

--- src/FormattedDateTime.php
+++ src/FormattedDateTime.php
@@ -8,41 +8,16 @@ class FormattedDateTime
 {
 
     /**
-     * Format datetime string.
-     *
-     * Supported $dateStamp formats:
-     * - YYYY-MM-DD HH:MM:SS
-     * - YYYY-MM-DD
-     * - MM/DD/YYYY
+     * Format datetime string
      *
      * @param string $dateStamp
      * @param string $format
      * @return string
      */
-    public static function x(string $dateStamp, string $format = 'M d, Y'): string
+    public static function x(string $srcDateTime, string $format): string
     {
-        // if date is YYYY-MM-DD HH:MM:SS
-        if (false !== strpos($dateStamp, ':')) {
-            $p = '~(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})~';
-            preg_match($p, $dateStamp, $matches);
+        $srcTimestamp = strtotime($srcDateTime);
 
-            $matches = array_map('intval', $matches);
-
-            $dateStamp = mktime($matches[4], $matches[5], $matches[6], $matches[2], $matches[3], $matches[1]);
-        }
-
-        // if date is YYYY-MM-DD
-        elseif (false !== strpos($dateStamp, '-')) {
-            list($y, $m, $d) = explode('-', $dateStamp);
-            $dateStamp = mktime(0, 0, 0, (int) $m, (int) $d, (int) $y);
-        }
-
-        // if date is MM/DD/YYYY
-        elseif (false !== strpos($dateStamp, '/')) {
-            list($m, $d, $y) = explode("/", $dateStamp);
-            $dateStamp = mktime(0, 0, 0, (int) $m, (int) $d, (int) $y);
-        }
-
-        return date($format, $dateStamp);
+        return date($format, $srcTimestamp);
     }
 }
